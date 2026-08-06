769f7687265748bc97b3921a8934b0b7e6bdb3b4
LincolnShow12 <lincolnmb89@gmail.com>
2026-08-05 21:10:25 -0300

    feat: adiciona endpoint POST /livros na API REST

--- src/main/java/acervo/api/ApiServer.java
+++ src/main/java/acervo/api/ApiServer.java
@@ -26,8 +26,8 @@ public class ApiServer {
         // Registra o adapter de LocalDate para o Gson conseguir converter
         // essas datas para JSON sem travar por causa da reflexão do Java
         this.gson = new GsonBuilder()
-        .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
-        .create();
+                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+                .create();
         this.app = Javalin.create();
 
         configurarRotas();
@@ -43,6 +43,16 @@ public class ApiServer {
             var livros = acervoService.getLivros();
             ctx.contentType("application/json");
             ctx.result(gson.toJson(livros));
+        });
+
+        // POST /livros -> cadastra um novo livro a partir do JSON enviado no corpo da requisição
+        app.post("/livros", ctx -> {
+            Livro novoLivro = gson.fromJson(ctx.body(), Livro.class);
+            acervoService.adicionarLivro(novoLivro);
+
+            ctx.contentType("application/json");
+            ctx.status(201); // 201 = "Created", código HTTP padrão para criação bem-sucedida
+            ctx.result(gson.toJson(novoLivro));
         });
     }
 
